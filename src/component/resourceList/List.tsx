e0f613238f9564532cab4f67f3510c7a2d695bc3
SunshineZerg <8208201214@csu.edu.cn>
2023-06-04 10:18:41 +0800

    feat: 搜索框调整并跑通

--- src/component/resourceList/List.tsx
+++ src/component/resourceList/List.tsx
@@ -1,14 +1,17 @@
 import { ResourceListItem } from './ListItem';
 import { IResource } from '@/domain/entity/resource.interface';
-import {Button, Form} from "@arco-design/web-react";
+import {Button, Form, Input} from "@arco-design/web-react";
 import {TypeSelect} from "@/component/resourceList/Select/TypeSelect";
 import {YearSelect} from "@/component/resourceList/Select/YearSelect";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Grid} from "@arco-design/web-react";
+import {throttle} from "lodash";
+import {wordPressCMS} from "@/services/cms/wp";
 const {Row,Col} = Grid;
 export const ResourceList = (props: { resources: IResource<any>[] }) => {
   const [resources,setResources] = useState(props.resources);
   const [form] = Form.useForm();
+  const [SearchValue,setSearchValue] = useState("");
 
   const isType = (typeArray:string[],target:string) => {
     let targeted = false;
@@ -27,13 +30,21 @@ export const ResourceList = (props: { resources: IResource<any>[] }) => {
     })
     return targeted;
   }
+  const throttledSearch = throttle((searchString: string) => {
+    wordPressCMS.fetchAll(searchString).then(res => setResources(res));
+  }, 1000);
+
+  useEffect(() => throttledSearch(SearchValue), [SearchValue]);
   const filter = () =>{
     const Search = form.getFieldsValue();
+    setSearchValue(Search.name);
+
+
     if(!Search.type&&!Search.year)
       setResources(props.resources);
     else {
       const newResources = new Array();
-      props.resources.map(resource => {
+      resources.map(resource => {
         // console.log(resource.type,"---",Search.type.type)
         console.log(isType(Search.type.type, resource.type));
         if (isType(Search.type.type, resource.type)) {
@@ -47,16 +58,23 @@ export const ResourceList = (props: { resources: IResource<any>[] }) => {
     <>
       <Form form={form}>
         <Row>
-          <Col span={8}>
+          <Col span={6}>
+            <Form.Item field="name" label="名称">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={2}/>
+          <Col span={6}>
             <Form.Item field="type" label="类型">
               <TypeSelect />
             </Form.Item >
           </Col>
-          <Col span={8}>
+          <Col span={6}>
             <Form.Item field="year" label="年份">
               <YearSelect />
             </Form.Item>
           </Col>
+
           <Col span={4}>
             <Button onClick={filter}>
               搜索
