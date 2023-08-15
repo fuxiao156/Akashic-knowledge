d9e46d904dfc02b1ccebcdd82f420e27c9e60a7c
SunshineZerg <8208201214@csu.edu.cn>
2023-08-16 02:21:32 +0800

    fix: 重置page

--- src/routes/page.tsx
+++ src/routes/page.tsx
@@ -1,25 +1,24 @@
-// 文件入口
+import { useEffect, useState } from 'react';
+import { throttle } from 'lodash';
+import { ResourceList } from '@/component/resourceList/List';
+import { IResource } from '@/domain/entity/resource.interface';
+import { wordPressCMS } from '@/services/cms/wp';
 
-import { Helmet } from '@modern-js/runtime/head';
-import RegisterPage from '@/routes/modules/RegisterPage';
-import MainPage from '@/routes/modules/MainPage';
-import SignInPage from '@/routes/modules/SignInPage';
+export default function Home() {
+  const [mediaList, setMediaList] = useState<IResource<any>[]>([]);
 
-const Index = () => (
-  <div>
-    <Helmet>
-      <link
-        rel="icon"
-        type="image/x-icon"
-        href="https://lf3-static.bytednsdoc.com/obj/eden-cn/uhbfnupenuhf/favicon.ico"
-      />
-    </Helmet>
-    <main>
-      {/* <SignInPage /> */}
-      <MainPage />
-      {/* <RegisterPage /> */}
+  const throttledSearch = throttle((searchString: string) => {
+    wordPressCMS.fetchAll(searchString).then(res => setMediaList(res));
+  }, 1000);
+
+  useEffect(() => throttledSearch(''), []);
+
+  return (
+    <main className="min-h-screen pt-32 flex flex-col items-start">
+      <div className="w-full top-0 p-5 fixed flex items-center gap-5 backdrop-blur-lg shadow-lg">
+        <h1 className="text-4xl font-bold">Akashic</h1>
+      </div>
+      <ResourceList resources={mediaList} />
     </main>
-  </div>
-);
-
-export default Index;
+  );
+}
