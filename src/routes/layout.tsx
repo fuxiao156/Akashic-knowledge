c4d4e6b35b3db56a433923fdb64dae1f55d4012f
rick <contactchienlin@gmail.com>
2023-09-17 20:19:19 +0800

    feat:--20230917 add home page

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,18 +1,21 @@
 import { Outlet } from '@modern-js/runtime/router';
-import {Tooltip} from "@arco-design/web-react";
-import {IconPlus} from "@arco-design/web-react/icon";
+import { Tooltip } from '@arco-design/web-react';
+import { IconPlus } from '@arco-design/web-react/icon';
+import './index.css';
 
 export default function Layout() {
   return (
     <>
-      <div className="w-full top-0 p-5 flex items-center gap-5 backdrop-blur-lg shadow-lg"style={{marginBottom:10}}>
-        <div className="text-4xl font-bold">Akashic
-        </div>
+      <div
+        className="w-full top-0 p-5 flex items-center gap-5 backdrop-blur-lg shadow-lg"
+        style={{ marginBottom: 10 }}
+      >
+        <div className="text-4xl font-bold">Akashic</div>
         <Tooltip content="上传资源">
-          <IconPlus></IconPlus>
+          <IconPlus />
         </Tooltip>
       </div>
-      <div>
+      <div className="w-full h-full">
         <Outlet />
       </div>
     </>
