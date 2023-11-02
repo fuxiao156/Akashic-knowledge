bd378a48e33c17307665f6fe26b3beb1e597d22e
Zency <2659255665@qq.com>
2023-11-02 20:21:07 +0800

    fix: 根据pr评论修改了商城页面的代码

--- src/routes/market/page.tsx
+++ src/routes/market/page.tsx
@@ -1,12 +1,24 @@
+// 展示商城主页
 import './index.css';
+import { Button, Grid, Input } from '@arco-design/web-react';
 
-// Title:标题 string
-// date:日期 string
-// price:价格 number
-// detaillink:详情页链接地址 string
-// buylink:购买页面链接地址 string
-// tipType:提示类型,0:即将上线,1:进行中,2:已结束 number
-const products = [
+const { GridItem } = Grid;
+const InputSearch = Input.Search;
+// Title:标题
+// date:日期
+// price:价格
+// detaillink:详情页链接地址
+// buylink:购买页面链接地址
+// tipType:提示类型,0:即将上线,1:进行中,2:已结束
+interface Product {
+  Title: string;
+  date: string;
+  price: number;
+  detailLink: string;
+  buyLink: string;
+  tipType: number;
+}
+const products: Product[] = [
   {
     Title: 'Product 1',
     date: '2021-01-01 00:00:00',
@@ -128,45 +140,45 @@ const products = [
     tipType: 2,
   },
 ];
-// tip的类型
-const tipType = ['future', 'now', 'past'];
+const tipClass = ['future', 'now', 'past'];
 const tipText = ['即将上线', '进行中', '已结束'];
 export default function Market() {
   return (
-    <div className="marketPage">
-      <div className="pageTitle">商城主页</div>
-      <div className="functionArea">
-        <div className="kindSelect">
+    <div className="market_page">
+      <div className="page-title">商城主页</div>
+      <div className="function-area">
+        <div className="kind-select">
           <div className="kind">资源类型1</div>
           <div className="kind">资源类型2</div>
         </div>
-        <div className="search">
-          <input className="searchInput" type="text" placeholder="搜索商品" />
-          <button className="searchButton">搜索</button>
-        </div>
+        <InputSearch allowClear placeholder="搜索" style={{ width: 288 }} />
       </div>
-      <div className="subTitle">商品</div>
-      <div className="sellBox">
+      <div className="sub-title">商品</div>
+      <Grid
+        cols={{ xs: 1, sm: 2, lg: 3, xl: 4, xxl: 5 }}
+        colGap={24}
+        rowGap={24}
+      >
         {products.map((product, index) => (
-          <div className="sellitem" key={index}>
-            <div className={`tip ${tipType[product.tipType]}`}>
+          <GridItem className="sellitem" key={index}>
+            <div className={`tip ${tipClass[product.tipType]}`}>
               {tipText[product.tipType]}
             </div>
-            <div className="itemTop">
-              <div className="itemTitle">{product.Title}</div>
+            <div className="item-top">
+              <div className="item-title">{product.Title}</div>
               <div className="date">{product.Title}</div>
             </div>
-            <div className="itemBottom">
-              <a className="detail" href={product.detailLink}>
+            <div className="item-bottom">
+              <Button style={{ marginRight: 20 }} type="primary">
                 详情
-              </a>
-              <a className="buy" href={product.buyLink}>
+              </Button>
+              <Button style={{ marginRight: 20 }} type="secondary">
                 购买
-              </a>
+              </Button>
             </div>
-          </div>
+          </GridItem>
         ))}
-      </div>
+      </Grid>
     </div>
   );
 }
